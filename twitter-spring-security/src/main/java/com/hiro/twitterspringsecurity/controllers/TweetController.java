dbf0a7c8c99166c3f62681e66b4703141222936f
Marcio Gabriel <gabriel09msn@gmail.com>
2024-03-27 18:34:51 -0300

    Update

--- twitter-spring-security/src/main/java/com/hiro/twitterspringsecurity/controllers/TweetController.java
+++ twitter-spring-security/src/main/java/com/hiro/twitterspringsecurity/controllers/TweetController.java
@@ -1,7 +1,5 @@
 package com.hiro.twitterspringsecurity.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
